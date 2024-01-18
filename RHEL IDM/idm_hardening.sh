33349cdd1860136d1d636062b7a432821d7c4bff
bvillano05 <bvillano05@gmail.com>
2024-01-17 19:47:51 -0500

    Added idm_hardening.sh

---
+++ RHEL IDM/idm_hardening.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Password prompt:
+echo "NOTICE: This script must be run on the IDM domain controller"
+read -s -p "Please enter password for IDM Domain Admin: " PASS < /dev/tty
+echo -e "\n" #Newline for formatting; can be removed if necessary
+echo $PASS|kinit admin #Get Kerberos ticket
+export HOSTNAME=$(hostname)
+#Disables anonymous LDAP binds from reading directory data while still allowing binds to root DSE information needed to get connection info:
+echo -e "dn: cn=config\nchangetype: modify\nreplace: nsslapd-allow-anonymous-access\nnsslapd-allow-anonymous-access: rootdse\n" | ldapmodify -x -D "cn=Directory Manager" -W -h $HOSTNAME -p 389 
+systemctl restart dirsrv.target #Restarts for changes to take effect
+
+
