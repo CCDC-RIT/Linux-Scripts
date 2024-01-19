fac719df1adec20be4350fee8317921153ec7e8c
bvillano05 <bvillano05@gmail.com>
2024-01-19 18:59:03 -0500

    more fixes

--- Initial/idm_hardening.sh
+++ Initial/idm_hardening.sh
@@ -41,14 +41,14 @@ display_options() {
 ALL(){
     export HOSTNAME=$(hostname)
     read -s -p "Please enter current password for IDM Domain Admin: " PASS < /dev/tty
+    read -s -p "Please enter current LDAP Directory Manager Password: " LDAP_PASS < /dev/tty
     echo $PASS|kinit admin #Get Kerberos ticket
-    echo -e "dn: cn=config\nchangetype: modify\nreplace: nsslapd-allow-anonymous-access\nnsslapd-allow-anonymous-access: rootdse\n" | ldapmodify -x -D "cn=Directory Manager" -w -H "ldaps://$HOSTNAME:636"
+    echo -e "dn: cn=config\nchangetype: modify\nreplace: nsslapd-allow-anonymous-access\nnsslapd-allow-anonymous-access: rootdse\n" | ldapmodify -x -D "cn=Directory Manager" -w $LDAP_PASS -H "ldaps://$HOSTNAME:636"
     ipa krbtpolicy-mod --maxlife=$((1*60*60)) --maxrenew=$((5*60*60)) #Change Kerberos global ticket policy time (max life of 1 hour and max renew of 5 hours)
 
     echo -e "\n" #Newline for formatting; can be removed if necessary
     read -s -p "Please enter new password for IDM Domain Admin: " NEW_PASS < /dev/tty
     echo -e "$NEW_PASS\n$NEW_PASS"|ipa user-mod admin --password
-    read -s -p "Please enter current LDAP Directory Manager Password: " LDAP_PASS < /dev/tty
     read -s -p "Please enter new LDAP Directory Manager Password: " NEW_LDAP_PASS < /dev/tty
     echo -e "dn: cn=config\nchangetype: modify\nreplace: nsslapd-rootpw\nnsslapd-rootpw: $NEW_LDAP_PASS\n\n"|ldapmodify -x -H "ldaps://$HOSTNAME:636" -D "cn=directory manager" -w $LDAP_PASS
 
