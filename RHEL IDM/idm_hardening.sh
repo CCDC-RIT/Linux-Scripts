b18a599f8fd3e74b3d1256f20959e22ee3e52f4a
bvillano05 <bvillano05@gmail.com>
2024-01-18 19:55:28 -0500

    Added password change using LDAPS for LDAP Directory Admin

--- RHEL IDM/idm_hardening.sh
+++ RHEL IDM/idm_hardening.sh
@@ -7,13 +7,14 @@ echo -e "\n" #Newline for formatting; can be removed if necessary
 echo $PASS|kinit admin #Get Kerberos ticket
 export HOSTNAME=$(hostname)
 #Disables anonymous LDAP binds from reading directory data while still allowing binds to root DSE information needed to get connection info (using LDAPS):
-echo -e "dn: cn=config\nchangetype: modify\nreplace: nsslapd-allow-anonymous-access\nnsslapd-allow-anonymous-access: rootdse\n" | ldapmodify -x -D "cn=Directory Manager" -W -H "ldaps://$HOSTNAME:636"
+echo -e "dn: cn=config\nchangetype: modify\nreplace: nsslapd-allow-anonymous-access\nnsslapd-allow-anonymous-access: rootdse\n" | ldapmodify -x -D "cn=Directory Manager" -w -H "ldaps://$HOSTNAME:636"
 ipa krbtpolicy-mod --maxlife=$((1*60*60)) --maxrenew=$((5*60*60)) #Change Kerberos global ticket policy time (max life of 1 hour and max renew of 5 hours)
 
 read -s -p "Please enter new password for IDM Domain Admin: " NEW_PASS < /dev/tty
 echo -e "$NEW_PASS\n$NEW_PASS"|ipa user-mod admin --password
 read -s -p "Please enter current LDAP Directory Manager Password: " LDAP_PASS < /dev/tty
 read -s -p "Please enter new LDAP Directory Manager Password: " NEW_LDAP_PASS < /dev/tty
+echo -e "dn: cn=config\nchangetype: modify\nreplace: nsslapd-rootpw\nnsslapd-rootpw: $NEW_LDAP_PASS\n\n"|ldapmodify -x -H "ldaps://$HOSTNAME:636" -D "cn=directory manager" -w $LDAP_PASS
 
 systemctl restart dirsrv.target #Restarts service for changes to take effect
 service krb5kdc restart #restarts Kerberos for changes to take effect
