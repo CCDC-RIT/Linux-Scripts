92af462e1630d15d643d60c97c6230fa64542655
Hal Williams <90720910+hfw8271@users.noreply.github.com>
2024-01-19 21:39:56 -0500

    justins a loser

--- Initial/Hardening Script/hardening.sh
+++ Initial/Hardening Script/hardening.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# by: Justin Huang (jznn) and Hal Williams (hfw8271)
+# by: Hal Williams (hfw8271) and Justin Huang (jznn)
 # Prerequisites: run as root, and users.txt file exists in same dir, can be created using getUsers.sh in setup folder
 
 # check if /etc/redhat-release file exists, indicating a RHEL-based system
